7113f033998e0062c1fec17a83a31f3ace3ffe40
sambhav <db.sambhav@gmail.com>
2026-04-23 15:29:43 +0530

    add formet date by akash

--- src/pages/PropertyManagement/ViewProperty.jsx
+++ src/pages/PropertyManagement/ViewProperty.jsx
@@ -53,9 +53,16 @@ const ViewListing = () => {
       return;
     }
 
-    const formattedStartDate = (approveDateTime) => {
-      const d = new Date(approveDateTime);
-      return d.toLocaleString();
+    const formattedStartDate = (dateTimeStr) => {
+      const d = new Date(dateTimeStr);
+      const day = String(d.getDate()).padStart(2, "0");
+      const month = String(d.getMonth() + 1).padStart(2, "0");
+      const year = d.getFullYear();
+      const hours = String(d.getHours()).padStart(2, "0");
+      const minutes = String(d.getMinutes()).padStart(2, "0");
+      const seconds = String(d.getSeconds()).padStart(2, "0");
+
+      return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
     };
 
     try {
